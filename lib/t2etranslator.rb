b9f4988a25590caeaee967c178afc3ed8b1ad59c
Sathia <sathia2704@gmail.com>
2012-04-07 12:07:55 +0530

    added soccet error exception

--- lib/t2etranslator.rb
+++ lib/t2etranslator.rb
@@ -3,9 +3,13 @@ require 'nokogiri'
 require 'open-uri'
 class Translator
   def self.translate(context, default = "ta")
-    page = Nokogiri::HTML(open(URI.parse(URI.encode("http://translate.google.com/?langpair=en|#{default}&text=#{context}"))))
-    result = page.search("#result_box")
-    result.text
+    begin
+		page = Nokogiri::HTML(open(URI.parse(URI.encode("http://translate.google.com/?langpair=en|#{default}&text=#{context}"))))
+		result = page.search("#result_box")
+		result.text
+	rescue SocketError
+		context
+	end
   end
 end
 
