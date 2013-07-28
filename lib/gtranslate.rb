c0cb1a11b0a06574f7210c3b21015b21a20237e5
sathia27 <sathia2704@gmail.com>
2013-07-28 19:29:07 +0530

    modularizing code

--- lib/gtranslate.rb
+++ lib/gtranslate.rb
@@ -1,22 +1,5 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
-String.class_eval do
-  def translate_to language
-    begin
-		  code = code_for language
-      page = Nokogiri::HTML(open(URI.parse(URI.encode("http://translate.google.com/?langpair=en|#{code}&text=#{self}"))))
-		  result = page.search("#result_box")
-		  result.text
-    rescue => e
-      puts e
-      puts 'Some problem to translate. Please try again later'
-      exit
-    end
-  end
-  def code_for language
-    language = language.downcase
-    language_codes = {'tamil' => 'ta', 'bengali' => 'bn', 'telugu' => 'te', 'malayalam' => 'ml', 'kannada' => 'kn', 'gujarati' => 'gu', 'hindi' => 'hi'}
-    language_codes[language]
-  end
-end
+require_relative 'gtranslate/string.rb'
+require_relative 'gtranslate/api.rb'
